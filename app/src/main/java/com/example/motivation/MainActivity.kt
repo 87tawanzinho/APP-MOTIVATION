359c344a5678b760a6f7c36f11e14d1f3e3ce504
tawan <87tawanzinho@gmail.com>
2023-12-24 10:55:56 -0300

    button click

--- app/src/main/java/com/example/motivation/MainActivity.kt
+++ app/src/main/java/com/example/motivation/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.motivation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.motivation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        binding.newPhraseButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if(v.id == R.id.new_phrase_button) {
+            Toast.makeText(this, "Progress", Toast.LENGTH_LONG).show()
+        }
     }
 }
