a814bcb2d208c90d88182f87ee4632ba2401030c
tawan <87tawanzinho@gmail.com>
2023-12-24 12:23:17 -0300

    new activity

---
+++ app/src/main/java/com/example/motivation/UserActivity.kt
@@ -0,0 +1,36 @@
+package com.example.motivation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.motivation.databinding.ActivityUserBinding
+
+class UserActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityUserBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSave.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_save) {
+            handleSave()
+        }
+    }
+
+    private fun handleSave() {
+        val name = binding.editName.text.toString()
+
+        if (name != "") {
+           startActivity( Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            Toast.makeText(this, "Você precisa de um nome", Toast.LENGTH_LONG).show()
+        }
+    }
+}
